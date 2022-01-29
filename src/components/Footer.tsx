00e9b69838604d99899dc2bf1536f2a91b248e97
pfelto <paul.r.felton@gmail.com>
2022-01-29 08:37:30 -0500

    Style and layout is looking decent. Think this will be a good stop and will come back to this later when I am completing my production Portfolio site in a few weeks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,27 +2,63 @@ import React from "react";
 import { SocialIcon } from "react-social-icons";
 
 export const Footer = ({ iconColor }) => {
-  const size = { height: 40, width: 40 };
+  const size = { height: 32, width: 32 };
   return (
-    <footer>
-      <SocialIcon
-        bgColor={iconColor}
-        style={size}
-        url="https://twitter.com/felton_paul"
-        target="_blank"
-      />
-      <SocialIcon
-        bgColor={iconColor}
-        style={size}
-        url="https://github.com/pfelto"
-        target="_blank"
-      />
-      <SocialIcon
-        bgColor={iconColor}
-        style={size}
-        url="https://app.pluralsight.com/profile/pfelton"
-        target="_blank"
-      />
+    <footer
+      style={{ display: "flex", flexDirection: "column", paddingTop: ".5vh" }}
+    >
+      <div>
+        <hr
+          style={{
+            color: iconColor,
+            width: "100%",
+          }}
+        ></hr>
+      </div>
+      <div style={{ paddingTop: ".5vh" }}>
+        <div className="iconHolder" style={{ display: "flex" }}>
+          <div>
+            <SocialIcon
+              bgColor={iconColor}
+              style={size}
+              url="https://github.com/pfelto"
+              target="_blank"
+            />
+          </div>
+          <div>
+            <SocialIcon
+              bgColor={iconColor}
+              style={size}
+              url="mailto:paul.r.felton@gmail.com"
+              target="_blank"
+            />
+          </div>
+          <div>
+            <SocialIcon
+              bgColor={iconColor}
+              style={size}
+              url="https://twitter.com/felton_paul"
+              target="_blank"
+            />
+          </div>
+          <div>
+            <SocialIcon
+              bgColor={iconColor}
+              style={size}
+              url="https://www.linkedin.com/in/paul-r-felton/"
+              target="_blank"
+            />
+          </div>
+          <div>
+            <SocialIcon
+              bgColor={iconColor}
+              style={size}
+              url="https://app.pluralsight.com/profile/pfelton"
+              target="_blank"
+            />
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
